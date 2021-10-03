6da154a49f5fff5908e3de3d8678d8f3ac9ec4a1
Brendan Chabannes <brendan.chabannes@hey.com>
2021-10-03 09:42:15 +0200

    handle everything in arrow functions

--- plf2022.js
+++ plf2022.js
@@ -32,162 +32,118 @@ function printResult(montant) {
   document.getElementById('budgetPourCent').textContent = (Math.round(pourcentageBudget * 100) / 100).toLocaleString();
 }
 
-// Gestion des stagiaires
-function remunererStagiaires(case_cochee) {
-  let budgetStagiaires = (stagiairesPe * salaireBrutMoy1d) + (stagiaires2d * salaireBrutMoy2d);
-  if (case_cochee.checked) {
-    augmentationBudget += budgetStagiaires;
-  } else {
-    augmentationBudget -= budgetStagiaires;
-  }
-  printResult(augmentationBudget);
-}
-
-// Gestion des suppressions de postes dans le 2d degré
-function suppressionPostes2d(case_cochee) {
-  let cout8000postes2d = suppressionPostesBlanquer2d * salaireBrutMoy2d;
-  if (case_cochee.checked) {
-    augmentationBudget += cout8000postes2d;
-  } else {
-    augmentationBudget -= cout8000postes2d;
-  }
-  printResult(augmentationBudget);
-}
-
-// Augmenter les postes de 10% le 2d degré
-function diminuerEffectifs2d(case_cochee) {
-  let coutProfsEnPlus = (enseignants2d / 10) * salaireBrutMoy2d;
-  if (case_cochee.checked) {
-    augmentationBudget += coutProfsEnPlus;
-  } else {
-    augmentationBudget -= coutProfsEnPlus;
-  }
-  printResult(augmentationBudget);
-}
-
-// Diminuer les effectifs dans le 1er degré
-function diminuerEffectifs1d(case_cochee) {
-  let coutPeEnPlus = enseignants1dBaisseEffectifs * salaireBrutMoy1d;
-  if (case_cochee.checked) {
-    augmentationBudget += coutPeEnPlus;
-  } else {
-    augmentationBudget -= coutPeEnPlus;
-  }
-  printResult(augmentationBudget);
-}
-
-// AESH à temps plein
-function aeshTempsPlein(case_cochee) {
-  let coutAeshActuel = salaireBrutMoyAesh * nbAesh;
-  let coutAeshTempsPlein = Math.round(coutAeshActuel * (nbAesh / etpAESH));
-  if (case_cochee.checked) {
-    augmentationBudget += coutAeshTempsPlein;
-  } else {
-    augmentationBudget -= coutAeshTempsPlein;
-  }
-  printResult(augmentationBudget);
-}
 
 
-// RASED
-function remonterRased(case_cochee) {
-  let budgetRased = rasedSupprimes * salaireBrutMoy1d;
-  if (case_cochee.checked) {
-    augmentationBudget += budgetRased;
-  } else {
-    augmentationBudget -= budgetRased;
-  }
-  printResult(augmentationBudget);
-}
-
-
-// Gestion des remplaçant⋅es 1d
-function augmenterRemplacement1d(case_cochee) {
-  let budgetRemplacement1d = (remplacantes1d * 0.25) * salaireBrutMoy1d;
-  if (case_cochee.checked) {
-    augmentationBudget += budgetRemplacement1d;
-  } else {
-    augmentationBudget -= budgetRemplacement1d;
-  }
-  printResult(augmentationBudget);
-}
-
-// Gestion référent⋅es égalité
-function refEgalite(case_cochee) {
-  let coutrefEgalite = departements * salaireBrutMoy2d;
-  if (case_cochee.checked) {
-    augmentationBudget += coutrefEgalite;
-  } else {
-    augmentationBudget -= coutrefEgalite;
-  }
-  printResult(augmentationBudget);
-}
-
-// Gestion référent⋅es écologie
-function refEcologie(case_cochee) {
-  let coutrefAntisexisme = departements * salaireBrutMoy2d;
-  if (case_cochee.checked) {
-    augmentationBudget += coutrefAntisexisme;
-  } else {
-    augmentationBudget -= coutrefAntisexisme;
-  }
-  printResult(augmentationBudget);
-}
 
 /* Gestionnaire d’événements */
 
 // Événement pour les suppressions de postes dans le 2d degré
 const stagiaires = document.querySelector('input[id="stagiaires"]');
 stagiaires.addEventListener("change", () => {
-  remunererStagiaires(stagiaires);
+  let budgetStagiaires = (stagiairesPe * salaireBrutMoy1d) + (stagiaires2d * salaireBrutMoy2d);
+  if (stagiaires.checked) {
+    augmentationBudget += budgetStagiaires;
+  } else {
+    augmentationBudget -= budgetStagiaires;
+  }
+  printResult(augmentationBudget);
 });
 
-// Événement pour les suppressions de postes dans le 2d degré
+// Suppressions de postes dans le 2d degré
 const suppr2d = document.querySelector('input[id="suppr2d"]');
 suppr2d.addEventListener("change", () => {
-  suppressionPostes2d(suppr2d);
+  let cout8000postes2d = suppressionPostesBlanquer2d * salaireBrutMoy2d;
+  if (suppr2d.checked) {
+    augmentationBudget += cout8000postes2d;
+  } else {
+    augmentationBudget -= cout8000postes2d;
+  }
+  printResult(augmentationBudget);
 });
 
 
-// Événement pour les réductions d’effectifs dans le 2d degré
+// Réductions d’effectifs dans le 2d degré
 const effectifs2d = document.querySelector('input[id="effectifs2d"]');
 effectifs2d.addEventListener("change", () => {
-  diminuerEffectifs2d(effectifs2d);
+  let coutProfsEnPlus = (enseignants2d / 10) * salaireBrutMoy2d;
+  if (effectifs2d.checked) {
+    augmentationBudget += coutProfsEnPlus;
+  } else {
+    augmentationBudget -= coutProfsEnPlus;
+  }
+  printResult(augmentationBudget);
 });
 
-//Événement pour les réductions d’effectifs dans le 1er degré
+// Réductions d’effectifs dans le 1er degré
 const effectifs1d = document.querySelector('input[id="effectifs1d"]');
 effectifs1d.addEventListener("change", () => {
-  diminuerEffectifs1d(effectifs1d);
+  let coutPeEnPlus = enseignants1dBaisseEffectifs * salaireBrutMoy1d;
+  if (effectifs1d.checked) {
+    augmentationBudget += coutPeEnPlus;
+  } else {
+    augmentationBudget -= coutPeEnPlus;
+  }
+  printResult(augmentationBudget);
 });
 
-// Événement pour les AESH à temps plein
+// AESH à temps plein
 const aesh = document.querySelector('input[id="aesh"]');
 aesh.addEventListener("change", () => {
-  aeshTempsPlein(aesh);
+  let coutAeshActuel = salaireBrutMoyAesh * nbAesh;
+  let coutAeshTempsPlein = Math.round(coutAeshActuel * (nbAesh / etpAESH));
+  if (aesh.checked) {
+    augmentationBudget += coutAeshTempsPlein;
+  } else {
+    augmentationBudget -= coutAeshTempsPlein;
+  }
+  printResult(augmentationBudget);
 });
 
 
-// Événement pour les RASED 
+// RASED 
 const rased = document.querySelector('input[id="rased"]');
 rased.addEventListener("change", () => {
-  remonterRased(rased);
+  let budgetRased = rasedSupprimes * salaireBrutMoy1d;
+  if (rased.checked) {
+    augmentationBudget += budgetRased;
+  } else {
+    augmentationBudget -= budgetRased;
+  }
+  printResult(augmentationBudget);
 });
 
 // Événement pour le remplacement 1er degré
 const remplacement1d = document.querySelector('input[id="remplacement1d"]');
 remplacement1d.addEventListener("change", () => {
-  augmenterRemplacement1d(remplacement1d);
+  let budgetRemplacement1d = (remplacantes1d * 0.25) * salaireBrutMoy1d;
+  if (remplacement1d.checked) {
+    augmentationBudget += budgetRemplacement1d;
+  } else {
+    augmentationBudget -= budgetRemplacement1d;
+  }
+  printResult(augmentationBudget);
 });
 
-// Évenement pour les refs antisexisme
+// 1 référent⋅e antisexisme par département
 const refantisexisme = document.querySelector('input[id="referenteEgalite"]');
 refantisexisme.addEventListener("change", () => {
-  refEgalite(refantisexisme);
+  let coutrefEgalite = departements * salaireBrutMoy2d;
+  if (refantisexisme.checked) {
+    augmentationBudget += coutrefEgalite;
+  } else {
+    augmentationBudget -= coutrefEgalite;
+  }
+  printResult(augmentationBudget);
 });
 
-// Évenement pour les refs écologie
+// 1 référent⋅e écologie par département
 const refecolo = document.querySelector('input[id="referenteEcologie"]');
 refecolo.addEventListener("change", () => {
-  refEcologie(refecolo);
+  let coutrefAntisexisme = departements * salaireBrutMoy2d;
+  if (refecolo.checked) {
+    augmentationBudget += coutrefAntisexisme;
+  } else {
+    augmentationBudget -= coutrefAntisexisme;
+  }
+  printResult(augmentationBudget);
 });
